80da515ff065de8b84ab3b581ff0105894dc0ce2
Krishna Ganguri <krishnakanth876@gmail.com>
2025-03-01 17:26:12 -0600

    js update

---
+++ functions/earlyAccess.js
@@ -0,0 +1,34 @@
+const faunadb = require('faunadb');
+
+const q = faunadb.query;
+const client = new faunadb.Client({
+    secret: process.env.FAUNA_SECRET_KEY, // Use environment variable
+});
+
+exports.handler = async (event) => {
+    if (event.httpMethod !== 'POST') {
+        return { statusCode: 405, body: 'Method Not Allowed' };
+    }
+
+    try {
+        const data = JSON.parse(event.body);
+        const email = data.email;
+        console.log('email: ', email);
+        await client.query(
+            q.Create(q.Collection('EarlyAccess'), {
+                data: { email: email },
+            })
+        );
+        console.log('Success: Early Access email added to FaunaDB');
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Success' }),
+        };
+    } catch (error) {
+        console.error('FaunaDB Error:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Internal Server Error' }),
+        };
+    }
+};
